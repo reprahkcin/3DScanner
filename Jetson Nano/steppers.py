732a99a92eae4449e867123577618260760bb330
Nick Harper <bignickharper@gmail.com>
2023-01-22 00:18:30 -0800

    update stepper script

--- Jetson Nano/steppers.py
+++ Jetson Nano/steppers.py
@@ -1,62 +1,68 @@
-import RPi.GPIO as GPIO
-import keyboard
+# Import the required libraries
+from gpiozero import LED, Button
+from time import sleep
 
-# Stepper A
-StepperA_EnablePin = 5
-StepperA_DirPin = 6
-StepperA_StepPin = 13
+# Define the GPIO pins for the stepper motor A
+enable_pin_A = LED(5)
+direction_pin_A = LED(6)
+step_pin_A = LED(13)
 
-# Stepper B
-StepperB_EnablePin = 17
-StepperB_DirPin = 27
-StepperB_StepPin = 22
+# Define the GPIO pins for the stepper motor B
+enable_pin_B = LED(17)
+direction_pin_B = LED(27)
+step_pin_B = LED(22)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(StepperA_EnablePin, GPIO.OUT)
-GPIO.setup(StepperA_DirPin, GPIO.OUT)
-GPIO.setup(StepperA_StepPin, GPIO.OUT)
-GPIO.setup(StepperB_EnablePin, GPIO.OUT)
-GPIO.setup(StepperB_DirPin, GPIO.OUT)
-GPIO.setup(StepperB_StepPin, GPIO.OUT)
+# Enable the stepper motors
+enable_pin_A.on()
+enable_pin_B.on()
 
-GPIO.output(StepperA_EnablePin, GPIO.HIGH)
-GPIO.output(StepperB_EnablePin, GPIO.HIGH)
+# Function to move stepper A in one direction
+def move_stepper_A_forward():
+    direction_pin_A.on()
+    step_pin_A.on()
+    sleep(0.01)
+    step_pin_A.off()
 
-def move_stepperA(direction):
-    if direction == "up":
-        # Set the direction of the motor
-        GPIO.output(StepperA_DirPin, GPIO.HIGH)
-        GPIO.output(StepperA_StepPin, GPIO.HIGH)
-        GPIO.output(StepperA_StepPin, GPIO.LOW)
-    elif direction == "down":
-        # Set the opposite direction of the motor
-        GPIO.output(StepperA_DirPin, GPIO.LOW)
-        GPIO.output(StepperA_StepPin, GPIO.HIGH)
-        GPIO.output(StepperA_StepPin, GPIO.LOW)
+# Function to move stepper A in the opposite direction
+def move_stepper_A_backward():
+    direction_pin_A.off()
+    step_pin_A.on()
+    sleep(0.01)
+    step_pin_A.off()
 
-def move_stepperB(direction):
-    if direction == "right":
-        # Set the direction of the motor
-        GPIO.output(StepperB_DirPin, GPIO.HIGH)
-        GPIO.output(StepperB_StepPin, GPIO.HIGH)
-        GPIO.output(StepperB_StepPin, GPIO.LOW)
-    elif direction == "left":
-        # Set the opposite direction of the motor
-        GPIO.output(StepperB_DirPin, GPIO.LOW)
-        GPIO.output(StepperB_StepPin, GPIO.HIGH)
-        GPIO.output(StepperB_StepPin, GPIO.LOW)
+# Function to move stepper B in one direction
+def move_stepper_B_forward():
+    direction_pin_B.on()
+    step_pin_B.on()
+    sleep(0.01)
+    step_pin_B.off()
 
+# Function to move stepper B in the opposite direction
+def move_stepper_B_backward():
+    direction_pin_B.off()
+    step_pin_B.on()
+    sleep(0.01)
+    step_pin_B.off()
+
+# Main loop
 while True:
-    if keyboard.is_pressed("up"):
-        move_stepperA("up")
-    elif keyboard.is_pressed("down"):
-        move_stepperA("down")
-    elif keyboard.is_pressed("right"):
-        move_stepperB("right")
-    elif keyboard.is_pressed("left"):
-        move_stepperB("left")
-    elif keyboard.is_pressed("esc"):
+    # Check for keyboard input
+    key = input("Press arrow keys to move stepper motors (or 'q' to quit): ")
+
+    # Check the input and call the appropriate function
+    if key == 'up':
+        move_stepper_A_forward()
+    elif key == 'down':
+        move_stepper_A_backward()
+    elif key == 'right':
+        move_stepper_B_forward()
+    elif key == 'left':
+        move_stepper_B_backward()
+    elif key == 'q':
         break
+    else:
+        print("Invalid input")
 
-GPIO.cleanup()
-
+# Disable the stepper motors
+enable_pin_A.off()
+enable_pin_B.off()
